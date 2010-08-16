c3b7f8867e2ebae4d535b8dfe790e94a3d440217
henryjiang <henry.jiang@sfgov.org>
2010-08-16 09:48:38 -0700

    minor detail updtaed

--- CreateServiceRequest.php
+++ CreateServiceRequest.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * 
+ *
  * Copyright 2010 Mark J. Headd
  * http://www.voiceingov.org
  *
  * This file is part of PHPOpen311
- * 
+ *
  * PHPOpen311 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,10 +16,10 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with PHPOpen311.  If not, see <http://www.gnu.org/licenses/>.
- *  
+ *
  */
 
 /**
@@ -31,7 +31,7 @@ include('classes/PHPOpen311.php');
 
 define("BASE_URL", "");
 define("API_KEY", "");
-define("CITY_ID", ""); 
+define("CITY_ID", "");
 
 // Service request information.
 $service_code = '021';
@@ -49,24 +49,24 @@ $description = 'There is a major pothole at this location.';
 $media_url = 'http://sf.streetsblog.org/wp-content/uploads/2009/06_18/bike.route.pothole.jpg';
 
 try {
-	
+
 	// Create a new instance of the Open 311 class.
 	$open311 = new Open311(BASE_URL, API_KEY, CITY_ID);
-	
+
 	// Create a new 311 Service request.
-	$open311->createRequest($service_code, $lat, $lon, $address_string, $address_id, $customer_email, $device_id, 
-				$account_id, $first_name, $last_name, $phone_number, $description, $media_url);	
-							
+	$open311->createRequest($service_code, $lat, $lon, $address_string, $address_id, $customer_email, $device_id,
+				$account_id, $first_name, $last_name, $phone_number, $description, $media_url);
+
 	$createRequestXML = new SimpleXMLElement($open311->getOutput());
 
 	// Check to see if an error code and message were returned.
 	if(strlen($createRequestXML->open311_error->errorCode) > 0) {
 		throw new create_requestException("API Error message returned: ".$createRequestXML->open311_error->errorDescription);
 	}
-	
+
 	// Display the ID of the service request.
 	//echo "Service Request ID: ".$createRequestXML->service_request->service_request_id;
-    echo "Service Request ID: ".$createRequestXML->service_request_id;
+    echo "Service Request ID: ".$createRequestXML->request->service_request_id;
 }
 
 catch (create_requestException $ex) {
